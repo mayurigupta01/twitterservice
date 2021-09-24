0cd2e2e894d45a82467400bf583c86be137563c1
Mayuri <mayurigupta@MBP.routerlogin.net>
2021-09-24 16:54:33 -0700

    tweet look api is implemented /lookuptwwet/<username>

--- twitterOptions/twitter_api/apiroutes.py
+++ twitterOptions/twitter_api/apiroutes.py
@@ -1,38 +1,43 @@
-from flask import Blueprint
-
+import json
+from flask import Blueprint, jsonify
+import requests
+from werkzeug.http import HTTP_STATUS_CODES
 
 twitter_api_blueprint = Blueprint("twitter_api", __name__, "url_prefix=/api/option")
+
 
 @twitter_api_blueprint.route('/', methods=['GET'])
 def landing_page():
     return "Welcome to HomePage"
 
 
+# Author-Mayuri(implement lookup tweets )
+@twitter_api_blueprint.route('/looktweet/<username>', methods=['GET'])
+def lookup_tweet(username):
+    # find the userid of the passed username
 
-#Author-Mayuri
-@twitter_api_blueprint.route('/looktweet', methods=['GET'])
-def search_tweet():
-
-    #find the userid of the passed username
     from helper.readyaml import read_yaml
     my_dict = read_yaml()
     from twitterOptions.twitter_api.UserOnTwitter import get_userid
-    user = get_userid('elonmusk')
-    userid= user['data']['id']
+    user = get_userid(username)
+    userid = user['data']['id']
 
-    #craete a request to fetch tweets and return response on web page
+    # create a request to fetch tweets and return response on web page- build a request that contains userid field.
+    my_headers = {'Authorization':
+                      'Bearer {}'.format(my_dict['credentials']['token'])}
+
+    response = requests.get(url="https://api.twitter.com/2/users/{}/tweets".format(userid), headers=my_headers)
+    tweets = response.json()
+    mytweetlist = tweets["data"]
+    tweet_dict = {}
+    counter = 1
+    for tweet in mytweetlist:
+        tweet_dict[counter] = tweet['text']
+        counter = counter + 1
+    return json.dumps(tweet_dict, indent=4)
 
 
-    return {"message": "searched tweet successfully",
-            "tokens":  my_dict['credentials']['token']
-            }
 
-
-# Author- Mayuri (implement post tweet)
-@twitter_api_blueprint.route('/create', methods=['POST'])
-def post_tweet():
-
-    return {"message": "created tweet successfully"}
 
 
 @twitter_api_blueprint.route('/update', methods=['PUT'])
@@ -43,3 +48,11 @@ def update_tweet():
 @twitter_api_blueprint.route('/delete', methods=['DELETE'])
 def delete():
     return {"message": "Delete Re-tweet successfully"}
+
+
+# Author-Mayuri
+def error_message(status_code, message):
+    payload = {'error': HTTP_STATUS_CODES.get(status_code, 'Unknown error')}
+    response = jsonify(payload)
+    response.status_code = status_code
+    return response
